2aae0446b13056265fdb08ff4951ef0c07941ee2
Harsh Dasila <74102378+harshdasila@users.noreply.github.com>
2024-02-05 20:15:09 +0530

    added talk to config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useRef } from 'react'
 import './App.css'
 import NavbarInfo from './components/NavbarInfo'
 import Navbar from './components/Navbar'
@@ -8,11 +8,23 @@ import DiscountsContainer from './components/DiscountsContainer'
 import Categories from './components/Categories'
 import Sales from './components/Sales'
 import FeaturedProducts from './components/FeaturedProducts'
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 function App() {
-  
+  const tawkMessengerRef = useRef();
+
+  const onLoad = () => {
+    console.log('onLoad works!');
+};
+
+  const handleMinimize = () => {
+    tawkMessengerRef.current.minimize();
+  };
+
   return (
     <>
+     <button onClick={handleMinimize}>Minimize the Chat</button>
+      <TawkMessengerReact propertyId="65c0f3c28d261e1b5f5c8f16" widgetId="default" onLoad={onLoad}/>
       <NavbarInfo/>
       <Navbar/>
       <MainPoster/>
@@ -20,6 +32,7 @@ function App() {
       <Categories/>
       <Sales/>
       <FeaturedProducts/>
+     
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <TawkTo propertyId='65c0f3c28d261e1b5f5c8f16' />
   </React.StrictMode>,
 )
